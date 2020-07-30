a6b95e3d53fe30ab90924fd4cbc8569f32913383
Jaque Laurenti <jaquelinelaurenti@gmail.com>
2020-07-30 13:39:45 -0300

    Iniciando front

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Home() {
   return (
-    <>
+    <Container>
       <Link to="/drones/list">Drones</Link>
       <br />
       <Link to="/mapa">Mapa</Link>
-    </>
+    </Container>
   );
 }
 
